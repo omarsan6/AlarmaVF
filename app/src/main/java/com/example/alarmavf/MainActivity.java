3812ebbdc90f8e69034b288225c663c44818e7ff
Omar <omarcore8>
2021-12-07 20:30:37 -0600

    Declaración de comentarios

---
+++ app/src/main/java/com/example/alarmavf/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.alarmavf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
